612463c8fccff542fc916e51325f1930aaca8ebf
jaeuuon <jaeuuon0157@outlook.com>
2024-04-11 17:55:21 +0900

    1. import 순서 정리.

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import reducer from './modules';
+
 import App from './App';
+
+import './index.css';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
