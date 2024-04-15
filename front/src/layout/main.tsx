4537ecf41acbe8cc24cdb228a5f1a2af9dd4d0fd
jaeuuon <jaeuuon0157@outlook.com>
2024-04-15 16:48:30 +0900

    1. 레이아웃 관련 작업중.

--- front/src/layout/main.tsx
+++ front/src/layout/main.tsx
@@ -1,35 +1,58 @@
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Fab from '@mui/material/Fab';
 import Grid from '@mui/material/Grid';
 
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const Main = () => {
-    const [isToTop, setToTop] = useState(false);
+    const sidebar = useRef<HTMLInputElement>(null);
 
-    const toTop = () => window.scroll({ top: 0, behavior: 'smooth' });
+    const [isToTop, setToTop] = useState(false);
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+    const onClickSidebar = () => {
+        setSidebarOpen(!isSidebarOpen);
+    }
 
     const handleScrollY = () => setToTop(window.scrollY >= 100 ? true : false);
+    const handleResize = () => {
+        if (sidebar?.current) {
+            const styles = window.getComputedStyle(sidebar.current);
+
+            if (styles.position !== 'fixed') {
+                setSidebarOpen(false);
+            }
+        }
+    };
 
     useEffect(() => {
         window.addEventListener('scroll', handleScrollY);
+        window.addEventListener('resize', handleResize);
 
-        return () => window.removeEventListener('scroll', handleScrollY);
+        return () => {
+            window.removeEventListener('scroll', handleScrollY);
+            window.removeEventListener('resize', handleResize);
+        }
     }, []);
 
     return (
-        <div id="div-layout-main">
-            <Grid id="grid-layout-main" container>
-                <Grid id="grid-layout-main-sidebar" item xs="auto">Sidebar</Grid>
-                <Grid item xs>Main</Grid>
-                {isToTop &&
-                    <Fab id="fab-layout-main-to-top" size="small" onClick={toTop}>
-                        <KeyboardArrowUpIcon />
-                    </Fab>
-                }
+        <Grid id="grid-layout-main" container>
+            <Grid id="grid-layout-main-sidebar" className={isSidebarOpen ? 'sidebar-open' : ''} item xs="auto" ref={sidebar}>
+                Sidebar
+                <div className="sidebar-icon" onClick={onClickSidebar}>
+                    {isSidebarOpen ? <KeyboardArrowLeftIcon /> : <KeyboardArrowRightIcon />}
+                </div>
             </Grid>
-        </div>
+            <Grid id="grid-layout-main-content" item xs>Main</Grid>
+            {isToTop &&
+                <div id="div-layout-main-to-top" onClick={() => window.scroll({ top: 0, behavior: 'smooth' })}>
+                    <KeyboardArrowUpIcon />
+                </div>
+            }
+        </Grid>
     );
 };
 
