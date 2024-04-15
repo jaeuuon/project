e9a0dc367916afd0230b46d400a6282a5ca6a8da
jaeuuon <jaeuuon0157@outlook.com>
2024-04-15 09:27:57 +0900

    1. 레이아웃 관련 작업중.

--- front/src/layout/header.tsx
+++ front/src/layout/header.tsx
@@ -1,6 +1,18 @@
+import { useState, useEffect } from 'react';
+
 const Header = () => {
+    const [isVisibleShadow, setVisibleShadow] = useState(false);
+
+    const handleScrollY = () => setVisibleShadow(window.scrollY > 0 ? true : false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScrollY);
+
+        return () => window.removeEventListener('scroll', handleScrollY);
+    }, []);
+
     return (
-        <div id="div-layout-header">
+        <div id="div-layout-header" className={isVisibleShadow ? 'shadow-bottom' : ''}>
             <div id="div-layout-header-content">Header</div>
         </div>
     );
