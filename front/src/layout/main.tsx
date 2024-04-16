db85541b597102d17beaa1fd427cd08b46d128d1
jaeuuon <jaeuuon0157@outlook.com>
2024-04-16 17:03:16 +0900

    1. 소스 정리.

--- front/src/layout/main.tsx
+++ front/src/layout/main.tsx
@@ -1,15 +1,36 @@
+import { useState, useEffect } from 'react';
+
 import Grid from '@mui/material/Grid';
+
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 import Sidebar from './main/sidebar';
 import Content from './main/content';
 
 const Main = () => {
+    const [isVisibleToTop, setVisibleToTop] = useState(false);
+
+    const onClickToTop = () => window.scroll({ top: 0, behavior: 'smooth' });
+
+    useEffect(() => {
+        const handleScrollY = () => setVisibleToTop(window.scrollY >= 100 ? true : false);
+
+        window.addEventListener('scroll', handleScrollY);
+
+        return () => window.removeEventListener('scroll', handleScrollY);
+    }, []);
+
     return (
         <div id="div-main">
             <Grid id="grid-main" container>
                 <Sidebar />
                 <Content />
             </Grid>
+            {isVisibleToTop &&
+                <div id="div-main-to-top" onClick={onClickToTop}>
+                    <KeyboardArrowUpIcon />
+                </div>
+            }
         </div>
     );
 };
