64547b90f59bdeecce43eb339c2b0259d15ff205
jaeuuon <jaeuuon0157@outlook.com>
2024-07-09 17:54:36 +0900

    [front] 1. 로그인 기록 조회 추가중.

--- front/src/pages/security/LoginHistory.tsx
+++ front/src/pages/security/LoginHistory.tsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
+
+import { Pagination } from '@mui/material';
+
+import type { Params } from 'types/apis/pages/security/loginHistory';
+
+import { list } from 'apis/pages/security/loginHistory';
+
 const LoginHistory = () => {
+    const [params, setParams] = useState<Params>({});
+
+    const onChange = (_event: React.ChangeEvent<unknown>, page: number) => {
+        setParams({ ...params, page });
+    };
+
     return (
-        <p>Login history</p>
+        <>
+            <Pagination count={10} shape="rounded" onChange={onChange} />
+        </>
     );
 };
 
