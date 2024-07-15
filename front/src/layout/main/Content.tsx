97699d1d2304931ead0216a8de10d52a9ff604dc
jaeuuon <jaeuuon0157@outlook.com>
2024-07-15 12:36:49 +0900

    [front] 1. user의 isInit과 경로의 IS_REQUIRED_INIT 따라 라우팅 렌더링되도록 수정.

--- front/src/layout/main/Content.tsx
+++ front/src/layout/main/Content.tsx
@@ -1,9 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
+import { useLocation, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { Grid } from '@mui/material';
 
 import { menu as headerMenu } from 'enums/layout/header/menu';
-import { menu as sidebarMenu } from 'enums/layout/main/sidebar';
+import { menu as sidebarMenu, menus as sidebarMenus } from 'enums/layout/main/sidebar';
+
+import type { RootState } from 'types/redux';
 
 import Home from 'pages/Home';
 import Notice from 'pages/home/Notice';
@@ -14,18 +17,32 @@ import Contact from 'pages/information/Contact';
 import Security from 'pages/Security';
 import LoginHistory from 'pages/security/LoginHistory';
 
+import Loading from 'components/Loading';
+
+const isRequiredInit = (pathname: string) => Object.values(headerMenu).some(({ PATH, IS_REQUIRED_INIT }) => PATH === pathname && IS_REQUIRED_INIT)
+    || sidebarMenus.some(({ SUB_MENUS }) => SUB_MENUS.some(({ PATH, IS_REQUIRED_INIT }) => PATH === pathname && IS_REQUIRED_INIT));
+
 const Content = () => {
+    const { pathname } = useLocation();
+
+    const { isInit } = useSelector((state: RootState) => state.user);
+
+    const isRenderRoutes = !isRequiredInit(pathname) || (isRequiredInit(pathname) && isInit);
+
     return (
         <Grid id="layout-main-grid-content" item xs>
             <div>
-                <Routes>
-                    <Route path={headerMenu.HOME.PATH} element={<Home />} />
-                    <Route path={sidebarMenu.HOME.NOTICE.PATH} element={<Notice />} />
-                    <Route path={headerMenu.INFORMATION.PATH} element={<Information />} />
-                    <Route path={sidebarMenu.INFORMATION.CONTACT.PATH} element={<Contact />} />
-                    <Route path={headerMenu.SECURITY.PATH} element={<Security />} />
-                    <Route path={sidebarMenu.SECURITY.HISTORY.PATH} element={<LoginHistory />} />
-                </Routes>
+                {isRenderRoutes &&
+                    <Routes>
+                        <Route path={headerMenu.HOME.PATH} element={<Home />} />
+                        <Route path={sidebarMenu.HOME.NOTICE.PATH} element={<Notice />} />
+                        <Route path={headerMenu.INFORMATION.PATH} element={<Information />} />
+                        <Route path={sidebarMenu.INFORMATION.CONTACT.PATH} element={<Contact />} />
+                        <Route path={headerMenu.SECURITY.PATH} element={<Security />} />
+                        <Route path={sidebarMenu.SECURITY.HISTORY.PATH} element={<LoginHistory />} />
+                    </Routes>
+                }
+                <Loading isVisible={!isRenderRoutes} />
             </div>
         </Grid>
     );
