fd56765ca75b0f5f9c3f1de056642517dc1faa76
jaeuuon <jaeuuon0157@outlook.com>
2024-07-22 10:09:49 +0900

    [back] 1. jwt 키를 snake에서 camel로 변경.

--- back/common-jwt/src/main/java/kr/jaeuuon/common/jwt/source/constant/JwtConstant.java
+++ back/common-jwt/src/main/java/kr/jaeuuon/common/jwt/source/constant/JwtConstant.java
@@ -9,6 +9,6 @@ public class JwtConstant {
     public static final String EMAIL_KEY = "email";
     public static final String NAME_KEY = "name";
     public static final String AUTHORITIES_KEY = "authorities";
-    public static final String AUTHORITY_VALUES_KEY = "authority_values";
+    public static final String AUTHORITY_VALUES_KEY = "authorityValues";
 
 }
