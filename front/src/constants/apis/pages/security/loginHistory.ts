f97c2ed6cffaf28556e52139f23cd6360a841cf3
jaeuuon <jaeuuon0157@outlook.com>
2024-07-25 10:17:05 +0900

    [front] 1. 소스 정리 - 상수 수정.

--- front/src/constants/apis/pages/security/loginHistory.ts
+++ front/src/constants/apis/pages/security/loginHistory.ts
@@ -5,9 +5,11 @@ export const QUERY = {
 } as const;
 
 export const COLUMN = {
-    REQUEST_IP: { KEY: 'requestIp', VALUE: 'Request IP', WIDTH: 280 },
-    RESULT: { KEY: 'result', VALUE: 'Result', WIDTH: 360 },
-    CREATED_TIME: { KEY: 'createdTime', VALUE: 'Created time', WIDTH: 440 }
+    REQUEST_IP: { key: 'requestIp', value: 'Request IP', width: 280 },
+    RESULT: { key: 'result', value: 'Result', width: 360 },
+    CREATED_TIME: { key: 'createdTime', value: 'Created time', width: 440 }
 } as const;
 
-export const COLUMNS = Object.values(COLUMN);
+export const COLUMNS = [
+    [COLUMN.REQUEST_IP, COLUMN.RESULT, COLUMN.CREATED_TIME]
+];
